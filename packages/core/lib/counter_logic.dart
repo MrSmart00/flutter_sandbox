222a2c2f26b9b9ecba9140429536993fec583947
MrSmart00 <nexearth@gmail.com>
2024-11-24 08:11:30 +0900

    Add: core package

---
+++ packages/core/lib/counter_logic.dart
@@ -0,0 +1,13 @@
+class CounterLogic {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+  }
+
+  void reset() {
+    _counter = 0;
+  }
+}
---
+++ packages/core/test/counter_logic_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+import '../lib/counter_logic.dart';
+
+void main() {
+  group('CounterLogic', () {
+    test('initial counter value should be 0', () {
+      final counterLogic = CounterLogic();
+      expect(counterLogic.counter, 0);
+    });
+
+    test('increment should increment counter value by 1', () {
+      final counterLogic = CounterLogic();
+      counterLogic.increment();
+      expect(counterLogic.counter, 1);
+    });
+
+    test('reset should set counter value to 0', () {
+      final counterLogic = CounterLogic();
+      counterLogic.increment();
+      counterLogic.reset();
+      expect(counterLogic.counter, 0);
+    });
+  });
+}
