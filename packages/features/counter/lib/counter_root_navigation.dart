86e2d9406197ba39e79d9a6a7ca28ea030a27682
MrSmart00 <nexearth@gmail.com>
2024-11-29 19:49:55 +0900

    Add: custom_lint

--- packages/features/counter/lib/counter_root_navigation.dart
+++ packages/features/counter/lib/counter_root_navigation.dart
@@ -2,30 +2,39 @@ import 'package:feature_counter/counter_page.dart';
 import 'package:feature_counter/stateless_counter_page.dart';
 import 'package:flutter/material.dart';
 
+/// A stateless widget that provides navigation to CounterPage and 
+/// StatelessCounterPage
 class CounterRootNavigation extends StatelessWidget {
+  /// Constructor for CounterRootNavigation
   const CounterRootNavigation({Key? key}) : super(key: key);
 
+  /// Builds the widget tree for this widget
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const CounterPage()),
-              );
-            },
-            child: const Text('Go to CounterPage'),
-          ),
+          /// Button to navigate to CounterPage
           ElevatedButton(
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => const StatelessCounterPage()),
+                  builder: (context) => const CounterPage(),
+                ),
+              );
+            },
+            child: const Text('Go to CounterPage'),
+          ),
+          /// Button to navigate to StatelessCounterPage
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const StatelessCounterPage(),
+                ),
               );
             },
             child: const Text('Go to StatelessCounterPage'),
--- packages/repositories/lib/entities/feed_item.dart
+++ packages/repositories/lib/entities/feed_item.dart
@@ -28,16 +28,18 @@ class FeedItem {
 
   factory FeedItem.fromJson(Map<String, dynamic> json) {
     return FeedItem(
-      title: json['title'],
-      pubDate: json['pubDate'],
-      link: json['link'],
-      guid: json['guid'],
-      author: json['author'],
-      thumbnail: json['thumbnail'],
-      description: json['description'],
-      content: json['content'],
-      enclosure: FeedEnclosure.fromJson(json['enclosure']),
-      categories: List<String>.from(json['categories']),
+      title: json['title'] as String,
+      pubDate: json['pubDate'] as String,
+      link: json['link'] as String,
+      guid: json['guid'] as String,
+      author: json['author'] as String,
+      thumbnail: json['thumbnail'] as String,
+      description: json['description'] as String,
+      content: json['content'] as String,
+      enclosure: FeedEnclosure.fromJson(
+        json['enclosure'] as Map<String, dynamic>,
+      ),
+      categories: List<String>.from(json['categories'] as Iterable<dynamic>),
     );
   }
 }
