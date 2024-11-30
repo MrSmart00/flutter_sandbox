8e238b80b8ff5d131634b0247dffd9961ef46152
MrSmart00 <nexearth@gmail.com>
2024-11-30 10:00:32 +0900

    Add: package for rss feature

--- packages/repositories/lib/services/network_client.dart
+++ packages/repositories/lib/services/network_client.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-import 'package:app_repository/entities/network_mode.dart';
-import 'package:app_repository/services/local_json_loader.dart';
+import 'package:app_repositories/entities/network_mode.dart';
+import 'package:app_repositories/services/local_json_loader.dart';
 import 'package:http/http.dart' as http;
 
 abstract class INetworkClient {
