b0ac09b684f56fe8afbf4aba198506d5beaeb016
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2024-12-12 11:41:10 +0900

    Add: sub directory for FlutterGen sample

--- packages/features/flutter_gen_sample/lib/gen/assets.gen.dart
+++ packages/features/flutter_gen_sample/lib/gen/assets.gen.dart
@@ -7,11 +7,23 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+class $AssetsJsonsGen {
+  const $AssetsJsonsGen();
+
+  /// File path: assets/jsons/hoge.json
+  String get hoge =>
+      'packages/feature_flutter_gen_sample/assets/jsons/hoge.json';
+
+  /// List of all assets
+  List<String> get values => [hoge];
+}
+
 class Assets {
   Assets._();
 
   static const String package = 'feature_flutter_gen_sample';
 
+  static const $AssetsJsonsGen jsons = $AssetsJsonsGen();
   static const String test =
       'packages/feature_flutter_gen_sample/assets/test.json';
 
